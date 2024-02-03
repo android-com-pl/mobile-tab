927d734285968f498111974b1dc7ef7d65a104f7
Rafał Całka <calka.rafal@gmail.com>
2024-02-03 21:52:11 +0100

    refactor: use generics for MobileTabItem attrs type

--- js/src/forum/components/MobileTabItem.tsx
+++ js/src/forum/components/MobileTabItem.tsx
@@ -9,9 +9,7 @@ interface MobileTabItemAttrs extends ComponentAttrs {
   label: string;
 }
 
-export default class MobileTabItem extends Component {
-  attrs!: MobileTabItemAttrs;
-
+export default class MobileTabItem extends Component<MobileTabItemAttrs> {
   view(vnode: Vnode<MobileTabItemAttrs, this>): Children {
     const { route, icon, label } = this.attrs;
     return (

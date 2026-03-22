c9aeda284feef79ab1202f37b1a9655edc504f71
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-03-23 00:51:50 +0100

    refactor: replace `extend` with `beforeMount`

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,14 +1,10 @@
-import Application from 'flarum/common/Application';
-import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import MobileTab from './components/MobileTab';
 
 export { default as extend } from './extend';
 
 app.initializers.add('acpl/mobile-tab', () => {
-  //@ts-ignore - missing 'mount' types
-  extend(Application.prototype, 'mount', () => {
-    const mTab = document.createElement('div');
-    m.mount(document.body.appendChild(mTab), MobileTab);
+  app.beforeMount(() => {
+    m.mount(document.body.appendChild(document.createElement('div')), MobileTab);
   });
 });
